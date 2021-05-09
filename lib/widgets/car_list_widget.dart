3f60c35dec6d92d78ef96d4dca7b834e10013a34
Yusuf Akkurt <6276592-yusuf-akkurt@users.noreply.gitlab.com>
2021-05-09 03:20:36 +0300

    Araba listesi tasarımı bitti

--- lib/widgets/car_list_widget.dart
+++ lib/widgets/car_list_widget.dart
@@ -14,31 +14,65 @@ class CarListWidget extends StatelessWidget {
 
   ListView buildCarList() {
     return ListView.builder(
-      itemCount: carDetails.length,
-      itemBuilder: (BuildContext context, index) {
-        var carDetail = this.carDetails[index];
-        var baseUrl = "https://10.0.2.2:5001/";
-        return Card(
-          margin: EdgeInsets.all(8),
-          child: Column(
-            children: <Widget>[
-              ListTile(
-                title: Text(carDetail.brandName + " " + carDetail.description),
-                subtitle: Text("Findex Puanı: " + carDetail.findexPoint.toString()),
-                trailing: Text(carDetail.dailyPrice.toString() + " ₺"),
-              ),
-              Image.network(baseUrl + carDetail.carImages[0].imagePath),
-              ListTile(
-                title: Text("Detaylar", style: TextStyle(fontWeight: FontWeight.bold),),
-                subtitle: Text(
-                  "Renk: " + carDetail.colorName +
-                  ", Model Yılı: " + carDetail.modelYear.toString(),
+        itemCount: carDetails.length,
+        itemBuilder: (BuildContext context, index) {
+          var carDetail = this.carDetails[index];
+          var baseUrl = "https://10.0.2.2:5001/";
+          return Card(
+            margin: EdgeInsets.all(8),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title:
+                      Text(carDetail.brandName + " " + carDetail.description),
+                  subtitle:
+                      Text("Findex Puanı: " + carDetail.findexPoint.toString()),
+                  trailing: Text(carDetail.dailyPrice.toString() + " ₺"),
                 ),
-              )
-            ],
-          ),
-        );
-      }
-    );
+                Image.network(baseUrl + carDetail.carImages[0].imagePath),
+                ListTile(
+                  title: Text(
+                    "Detaylar",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    "Renk: " +
+                        carDetail.colorName +
+                        ", Model Yılı: " +
+                        carDetail.modelYear.toString(),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.only(right: 8, bottom: 10),
+                        child: OutlinedButton(
+                          child: Text("Detaylar"),
+                          onPressed: () {
+                            print("Detaylar");
+                          },
+                          style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.all<Color>(Colors.black87),
+                        ),
+                        )),
+                    Padding(
+                        padding: EdgeInsets.only(right: 16, bottom: 10),
+                        child: OutlinedButton(
+                          child: Text("Kirala"),
+                          onPressed: () {
+                            print("Kirala");
+                          },
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(Colors.black54),
+                            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                        ),
+                        )),
+                  ],
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
