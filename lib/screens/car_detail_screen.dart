61c5d79bbc4969a0e3cda43cd700073bc0b5b5a1
Yusuf Akkurt <6276592-yusuf-akkurt@users.noreply.gitlab.com>
2021-05-12 21:55:46 +0300

    Detaylandırma yapıldı

--- lib/screens/car_detail_screen.dart
+++ lib/screens/car_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rent_a_car/models/car_detail.dart';
 
@@ -19,16 +20,25 @@ class CarDetailScreen extends StatelessWidget {
     );
   }
 
-  Widget buildBody() {
+  Widget buildBody(){
+    return Column(
+      children: <Widget>[
+        Expanded(child: buildSlider()),
+        Expanded(child: buildDetail()),
+      ],
+    );
+  }
+
+  Widget buildSlider() {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: ListView(
         children: [
           CarouselSlider.builder(
-            itemCount: carDetail.carImages.length,
+            itemCount: this.carDetail.carImages.length,
             itemBuilder: (context, index, realIdx) {
               var baseUrl = "https://10.0.2.2:5001/";
-              var carImage = carDetail.carImages[index];
+              var carImage = this.carDetail.carImages[index];
               return Container(
                 margin: EdgeInsets.all(6.0),
                 decoration: BoxDecoration(
@@ -48,6 +58,46 @@ class CarDetailScreen extends StatelessWidget {
               viewportFraction: 0.8,
             ),
           )
+        ],
+      ),
+    );
+  }
+
+  Widget buildDetail() {
+    return Container(
+      margin: EdgeInsets.all(8),
+      child: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text(this.carDetail.brandName + ", " + this.carDetail.description),
+            subtitle: Text("Marka, Model"),
+            trailing: Icon(Icons.local_taxi,color: Colors.grey,),
+          ),
+          ListTile(
+            title: Text(this.carDetail.modelYear.toString() + ", " + this.carDetail.colorName),
+            subtitle: Text("Model Yılı, Renk"),
+            trailing: Icon(Icons.color_lens_outlined,color: Colors.grey,),
+          ),
+          ListTile(
+            title: Text(this.carDetail.findexPoint.toString() + " FP, " + this.carDetail.dailyPrice.toString() + " ₺"),
+            subtitle: Text("Findex Puanı, Günlük ücret"),
+            trailing: Icon(Icons.money,color: Colors.grey,),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(right: 8, bottom: 10),
+                  child: OutlinedButton(
+                    child: Text("Kirala"),
+                    onPressed: () => print("Kirala"),
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          Colors.black87),
+                    ),
+                  )),
+            ],
+          ),
         ],
       ),
     );
